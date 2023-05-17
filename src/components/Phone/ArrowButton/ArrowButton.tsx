5e1b1e559a2e71e44ae021ac7b6516828862e6e6
Lazarev Ruslan <laruse91@gmail.com>
2023-05-18 00:06:17 +0300

    feat(hot_fix) - fixed  Phone input button bug

--- src/components/Phone/ArrowButton/ArrowButton.tsx
+++ src/components/Phone/ArrowButton/ArrowButton.tsx
@@ -1,10 +1,10 @@
-import { type FC } from "react";
+import { type FC } from 'react'
+import { ArrowRightIcon } from '../../../assets'
 
-import classes from "./ArrowButton.module.css";
-import arrowImage from "./arrow.svg";
+import classes from './ArrowButton.module.css'
 
 export const ArrowButton: FC<{ onClick?(): void }> = ({ onClick }) => (
   <button className={classes.arrowButton} onClick={onClick}>
-    <img className={classes.icon} src={arrowImage} alt="" />
+    <ArrowRightIcon/>
   </button>
-);
+)
